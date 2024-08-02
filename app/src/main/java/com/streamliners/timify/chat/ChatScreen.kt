0d3a148fe0aad7de2d15f7bbc7339c80d48dcb8e
Hey-Deepak <choudharydeepak990@gmail.com>
2024-08-03 04:41:42 +0530

    Gemini Model Added into ChatViewModel

--- app/src/main/java/com/streamliners/timify/chat/ChatScreen.kt
+++ app/src/main/java/com/streamliners/timify/chat/ChatScreen.kt
@@ -1,9 +1,13 @@
 package com.streamliners.timify.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -19,11 +23,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.ai.client.generativeai.type.asTextOrNull
+import com.streamliners.compose.android.comp.appBar.TitleBarScaffold
 
 @Composable
 fun ChatScreen(
@@ -32,71 +39,98 @@ fun ChatScreen(
 ) {
 
 
-    val placeholderPrompt = "Batao Kya kiya Aaj"
+    val placeholderPrompt = ""
     val placeholderResult = "Rusult Yaha aayega..."
     var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
     var result by rememberSaveable { mutableStateOf(placeholderResult) }
     val uiState by viewModel.uiState.collectAsState()
-    val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = "Timify",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(16.dp)
-        )
 
-        Row(
-            modifier = Modifier.padding(all = 16.dp)
+    TitleBarScaffold(
+        title = "Timify",
+        navigateUp = { navController.navigateUp() }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-            TextField(
-                value = prompt,
-                label = { Text("Prompt") },
-                onValueChange = { prompt = it },
-                modifier = Modifier
-                    .weight(0.8f)
-                    .padding(end = 16.dp)
-                    .align(Alignment.CenterVertically)
-            )
 
-            Button(
-                onClick = {
-                    viewModel.sendPrompt(prompt)
-                },
-                enabled = prompt.isNotEmpty(),
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-            ) {
-                Text(text ="Go")
+
+            if (uiState is UiState.Loading) {
+                Column (
+                    Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center
+                ){
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+
+                }
+            } else {
+                val scrollState = rememberScrollState()
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .verticalScroll(scrollState),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var textColor = MaterialTheme.colorScheme.onSurface
+                    if (uiState is UiState.Error) {
+                        textColor = MaterialTheme.colorScheme.error
+                        result = (uiState as UiState.Error).errorMessage
+                    } else if (uiState is UiState.Success) {
+                        textColor = MaterialTheme.colorScheme.onSurface
+                        result = (uiState as UiState.Success).outputText
+                    }
+
+                    viewModel.chatHistoryState.forEach { chat ->
+                        Text(
+                            text = chat.parts[0].asTextOrNull().toString(),
+                            textAlign = TextAlign.Start,
+                            color = textColor,
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(16.dp)
+                                .wrapContentSize()
+
+                        )
+                    }
+
+                }
             }
+
+            Column {
+                Row(
+                    modifier = Modifier
+                        .padding(all = 8.dp)
+                ) {
+                    TextField(
+                        value = prompt,
+                        label = { Text("Prompt") },
+                        onValueChange = { prompt = it },
+                        modifier = Modifier
+                            .weight(0.8f)
+                            .padding(end = 16.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+
+                    Button(
+                        onClick = {
+                            viewModel.sendPrompt(prompt)
+                            prompt = ""
+                        },
+                        enabled = prompt.isNotEmpty(),
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                    ) {
+                        Text(text = "Go")
+                    }
+                }
+            }
+
+
         }
 
-        if (uiState is UiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            if (uiState is UiState.Error) {
-                textColor = MaterialTheme.colorScheme.error
-                result = (uiState as UiState.Error).errorMessage
-            } else if (uiState is UiState.Success) {
-                textColor = MaterialTheme.colorScheme.onSurface
-                result = (uiState as UiState.Success).outputText
-            }
-            val scrollState = rememberScrollState()
-            Text(
-                text = result,
-                textAlign = TextAlign.Start,
-                color = textColor,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(16.dp)
-                    .fillMaxSize()
-                    .verticalScroll(scrollState)
-            )
-        }
     }
+
 
 }
 
--- app/src/main/java/com/streamliners/timify/chat/ChatViewModel.kt
+++ app/src/main/java/com/streamliners/timify/chat/ChatViewModel.kt
@@ -4,7 +4,9 @@ import com.streamliners.base.BaseViewModel
 
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.Content
 import com.google.ai.client.generativeai.type.content
+import com.google.ai.client.generativeai.type.generationConfig
 import com.streamliners.timify.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,9 +20,28 @@ class ChatViewModel : BaseViewModel() {
     val uiState: StateFlow<UiState> =
         _uiState.asStateFlow()
 
+
+    val chatHistoryState = mutableListOf<Content>()
+
     private val generativeModel = GenerativeModel(
-        modelName = "gemini-1.5-flash",
-        apiKey = BuildConfig.apiKey
+        "gemini-1.5-flash",
+        // Retrieve API key as an environmental variable defined in a Build Configuration
+        // see https://github.com/google/secrets-gradle-plugin for further instructions
+        BuildConfig.apiKey,
+        generationConfig = generationConfig {
+            temperature = 1f
+            topK = 64
+            topP = 0.95f
+            maxOutputTokens = 8192
+            responseMimeType = "text/plain"
+        },
+        // safetySettings = Adjust safety settings
+        // See https://ai.google.dev/gemini-api/docs/safety-settings
+        systemInstruction = content {
+            text(
+                "You are a time managing assistant, helping user in knowing where they spend most of their time. Also, providing analytics as to what percentage of time is spent on what tasks day / week / month wise. For this you have to converse with the user with sole aim of finding where and how they spent their day. With task, we aim to collect only the name of the task like - Writing blog, working on Foo project, going FooBar place, walking, etc. Ask questions like - “What were you working on recently?”, “With what task did you start your day with?”. When user. says Hi, start asking such questions and collect the data as to how they spent their day. After conversing, when prompted - “Give data”, you must provide the summary as to how user spent the day in this format :\n\n10 AM - 12 PM : A Project\n\n12 PM - 2 PM : Lunch and Rest\n\n2 PM - 4 PM : Code Review\n\n4 PM - 5 PM : Gym\n\n5 PM - 6 PM : Dinner\n\n6 PM - 7 PM : Walk\n\n7 PM - 9 PM : Meeting\n\nMake sure to keep collecting entire day’s data by asking relevant questions. Output must be of continuous time slots with what was done in each."
+            )
+        },
     )
 
     fun sendPrompt(
@@ -28,16 +49,26 @@ class ChatViewModel : BaseViewModel() {
     ) {
         _uiState.value = UiState.Loading
 
+
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val response = generativeModel.generateContent(
-                    content {
+                val response = generativeModel.startChat(chatHistoryState).sendMessage(prompt)
+
+                chatHistoryState.add(
+                    content("user") {
                         text(prompt)
                     }
                 )
+
                 response.text?.let { outputContent ->
+                    chatHistoryState.add(
+                        content("model") {
+                            text(outputContent)
+                        }
+                    )
                     _uiState.value = UiState.Success(outputContent)
                 }
+
             } catch (e: Exception) {
                 _uiState.value = UiState.Error(e.localizedMessage ?: "")
             }
