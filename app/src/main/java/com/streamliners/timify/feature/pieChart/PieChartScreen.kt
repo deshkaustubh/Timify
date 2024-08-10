5457417164c7c668db80af8d7e1c634e0d1281fb
Hey-Deepak <choudharydeepak990@gmail.com>
2024-08-10 06:08:32 +0530

    1. Data showing in PieChartScreen 2. Pie Chart ViewModel improved.

--- app/src/main/java/com/streamliners/timify/feature/pieChart/PieChartScreen.kt
+++ app/src/main/java/com/streamliners/timify/feature/pieChart/PieChartScreen.kt
@@ -4,12 +4,14 @@ import android.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.streamliners.base.taskState.comp.whenLoaded
 import com.streamliners.compose.android.comp.appBar.TitleBarScaffold
 import com.streamliners.timify.feature.chat.ChatViewModel
 import ir.mahozad.android.PieChart
@@ -17,31 +19,34 @@ import ir.mahozad.android.PieChart
 @Composable
 fun PieChartScreen(
     navController: NavController,
-    viewModel: ChatViewModel
+    viewModel: PieChartViewModel
 ) {
-    //val context = LocalContext.current
+
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getPieChartData()
+    }
 
     TitleBarScaffold(
         title = "Pie Chart",
         navigateUp = {navController.navigateUp()}) { it   ->
-        AndroidView(
-            modifier = Modifier.fillMaxSize().padding(it)
-                .padding(24.dp),
-            factory = { context ->
-                PieChart(context).apply {
-                    slices = listOf(
-                        PieChart.Slice(fraction = 0.2f, color = Color.BLUE, label = "ABC"),
-                        PieChart.Slice(fraction = 0.4f, color = Color.MAGENTA, label = "DEF"),
-                        PieChart.Slice(fraction = 0.3f, color = Color.GREEN, label = "GHI"),
-                        PieChart.Slice(fraction = 0.1f, color = Color.CYAN, label = "JKL")
-                    )
+        viewModel.slice.whenLoaded { s ->
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(24.dp),
+                factory = { context ->
+                    PieChart(context).apply {
+                        slices = s
+                    }
+                },
+                update = { view ->
+                    // View's been inflated or state read in this block has been updated
+                    // Add logic here if necessary
                 }
-            },
-            update = { view ->
-                // View's been inflated or state read in this block has been updated
-                // Add logic here if necessary
-            }
-        )
+            )
+        }
+
     }
 
 }
--- app/src/main/java/com/streamliners/timify/feature/pieChart/PieChartViewModel.kt
+++ app/src/main/java/com/streamliners/timify/feature/pieChart/PieChartViewModel.kt
@@ -1,11 +1,19 @@
 package com.streamliners.timify.feature.pieChart
 
+import android.graphics.Color
 import com.streamliners.base.BaseViewModel
+import com.streamliners.base.ext.execute
+import com.streamliners.base.taskState.taskStateOf
+import com.streamliners.base.taskState.update
 import com.streamliners.timify.TimifyApp
+import com.streamliners.timify.domain.PieChartInfo
+import ir.mahozad.android.PieChart
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
-class PieChartViewModel: BaseViewModel() {
+class PieChartViewModel : BaseViewModel() {
 
     val pieChartInfoDao = TimifyApp.localDB.pieChartInfoDao()
 
@@ -13,7 +21,62 @@ class PieChartViewModel: BaseViewModel() {
 
     val currentDate = LocalDateTime.now().format(formatter)
 
+    // TODO: Why it is in var
+    var listOfPieChartInfo = mutableListOf<PieChartInfo>()
+    val slice = taskStateOf<List<PieChart.Slice>>()
+
+    fun getPieChartData() {
+
+        execute {
+            listOfPieChartInfo = pieChartInfoDao.getPieChartInfo(currentDate).toMutableList()
+            setDate()
+        }
+
+    }
 
 
+    private fun calculateTimeDifference(start: String, end: String): Int {
+        // Define the date format
+        val format = SimpleDateFormat("hh a", Locale.getDefault())
+
+        // Parse the start and end times
+        val startDate = format.parse(start)
+        val endDate = format.parse(end)
+
+        // Calculate the difference in milliseconds
+        val differenceInMillis = endDate.time - startDate.time
+
+        // Convert milliseconds to hours
+        return (differenceInMillis / (1000 * 60 * 60)).toInt()
+    }
+
+    private fun fraction(start: String, end: String, totalHours: Int):Float{
+
+        val fraction: Float = calculateTimeDifference(start, end) * 1.0f / totalHours
+
+        return fraction
+    }
+
+
+    // TODO: Where should I call it?
+    fun setDate(){
+        execute {
+            val list = mutableListOf<PieChart.Slice>()
+            val totalHours = mutableListOf<Int>()
+            listOfPieChartInfo.forEach {
+                totalHours.add(
+                    calculateTimeDifference(it.startTime, it.endTime)
+                )
+            }
+            listOfPieChartInfo.forEach {
+                    list.add(
+                        PieChart.Slice(fraction = fraction(it.startTime, it.endTime, totalHours.sum()) ,color = Color.BLUE, label = it.taskName)
+                    )
+            }
+            slice.update(
+                list
+            )
+        }
+    }
 
 }
